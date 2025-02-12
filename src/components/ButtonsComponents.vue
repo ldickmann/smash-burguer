235010dc76c9eb2a96b70bba126b37ec156c399c
Lucas E. Dickmann <131892016+ldickmann@users.noreply.github.com>
2025-02-12 19:41:52 -0300

    ButtonsComponente add & filtro por buttons na page Cardapio

--- src/components/ButtonsComponents.vue
+++ src/components/ButtonsComponents.vue
@@ -1,12 +1,66 @@
 <template>
-  <div class="buttons-components">
-    <button class="button">Button</button>
-    <button class="button button--primary">Button Primary</button>
-    <button class="button button--secondary">Button Secondary</button>
+  <div class="container-buttons" :style="{ gap: `${gap}px` }">
+    <button
+      v-for="button in buttons"
+      :key="button.category"
+      class="button"
+      :style="customStyle"
+      @click="selectCategory(button.category)"
+    >
+      {{ button.label }}
+    </button>
   </div>
 </template>
 
-<script setup></script>
+<script setup>
+import { computed } from "vue";
+
+const props = defineProps({
+  buttons: {
+    type: Array,
+    required: true,
+  },
+  backgroundColor: {
+    type: String,
+    default: "#3498db",
+  },
+  fontColor: {
+    type: String,
+    default: "#fff",
+  },
+  fontSize: {
+    type: String,
+    default: "16px",
+  },
+  buttonSize: {
+    type: String,
+    default: "12px 24px",
+  },
+  borderRadius: {
+    type: String,
+    default: "4px",
+  },
+  gap: {
+    type: Number,
+    default: 8,
+  },
+});
+
+const emit = defineEmits(["category-selected"]);
+
+const selectCategory = (category) => {
+  emit("category-selected", category);
+};
+
+const customStyle = computed(() => ({
+  backgroundColor: props.backgroundColor,
+  color: props.fontColor,
+  fontSize: props.fontSize,
+  padding: props.buttonSize,
+  borderRadius: props.borderRadius,
+  gap: `${props.gap}px`,
+}));
+</script>
 
 <style scoped lang="scss">
 @use "@/assets/styles/components/buttons";
--- src/views/MenuPage.vue
+++ src/views/MenuPage.vue
@@ -8,9 +8,24 @@
   />
 
   <div class="menu-page">
+    <ButtonsComponents
+      :buttons="[
+        { label: 'Foods', category: 'foods' },
+        { label: 'Drinks', category: 'drinks' },
+        { label: 'Desserts', category: 'desserts' },
+      ]"
+      backgroundColor="#e74c3c"
+      fontColor="#fff"
+      fontSize="18px"
+      buttonSize="14px 28px"
+      borderRadius="8px"
+      :gap="18"
+      @category-selected="filterByCategory"
+    />
+
     <div class="menu">
       <BurguerCard
-        v-for="burger in burgers"
+        v-for="burger in filteredBurgers"
         :key="burger.id"
         :burger="burger"
         @add-to-cart="addToCart"
@@ -28,23 +43,55 @@ import { ref } from "vue";
 import { useCartStore } from "../store/cart";
 import BurguerCard from "../components/BurguerCard.vue";
 import BannerComponent from "@/components/BannerComponent.vue";
+import ButtonsComponents from "@/components/ButtonsComponents.vue";
 
 const cartStore = useCartStore();
 
-const burgers = ref([
-  { id: 1, name: "Cheese Bacon", price: 25.99, image: "./images/foods/cheese-bacon.jpg" },
-  { id: 2, name: "Smash Burger", price: 22.5, image: "./images/foods/smash-burger.jpg" },
+const allBurgers = ref([
+  {
+    id: 1,
+    name: "Cheese Bacon",
+    price: 25.99,
+    image: "./images/foods/cheese-bacon.jpg",
+    category: "foods",
+  },
+  {
+    id: 2,
+    name: "Smash Burger",
+    price: 22.5,
+    image: "./images/foods/smash-burger.jpg",
+    category: "foods",
+  },
   {
     id: 3,
     name: "Veggie Burger",
     price: 24.9,
     image: "./images/foods/veggie-burger.jpg",
+    category: "foods",
   },
-  { id: 4, name: "X-Salada", price: 20.9, image: "./images/foods/x-salada.jpg" },
+  {
+    id: 4,
+    name: "X-Salada",
+    price: 20.9,
+    image: "./images/foods/x-salada.jpg",
+    category: "foods",
+  },
 ]);
+
+const filteredBurgers = ref([...allBurgers.value]);
 
 const addToCart = (burger) => {
   cartStore.addItem(burger);
+};
+
+const filterByCategory = (category) => {
+  if (category === "all") {
+    filteredBurgers.value = [...allBurgers.value];
+  } else {
+    filteredBurgers.value = allBurgers.value.filter(
+      (burger) => burger.category === category
+    );
+  }
 };
 </script>
 
