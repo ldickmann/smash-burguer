ad28ba4425db6f3d15a8f17fd081db854cf367dd
ldickmann <ldickmann12@gmail.com>
2025-02-26 15:30:30 -0300

    STYLE: Responsividade footer-admin

--- src/components/admin/layout/TheFooterAdmin.vue
+++ src/components/admin/layout/TheFooterAdmin.vue
@@ -1,16 +1,18 @@
 <template>
   <footer class="admin-footer">
-    <div class="container-copy">
+    <div class="container-copy-admin">
       <p>Copyright © 2025 Smash Burgers</p>
 
       <div class="developer-info">
         <p>Web Developer | Lucas E. Dickmann</p>
-        <a href="https://github.com/ldickmann" target="_blank">
-          <font-awesome-icon :icon="['fab', 'github']" />
-        </a>
-        <a href="https://linkedin.com/in/lucas-dickmann" target="_blank">
-          <font-awesome-icon :icon="['fab', 'linkedin']" />
-        </a>
+        <div class="social-links">
+          <a href="https://github.com/ldickmann" target="_blank">
+            <font-awesome-icon :icon="['fab', 'github']" />
+          </a>
+          <a href="https://linkedin.com/in/lucas-dickmann" target="_blank">
+            <font-awesome-icon :icon="['fab', 'linkedin']" />
+          </a>
+        </div>
       </div>
     </div>
   </footer>
