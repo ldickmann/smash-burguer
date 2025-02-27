43af65b21d27be5b5dc6c120039bc139e82b2a7a
ldickmann <ldickmann12@gmail.com>
2025-02-27 06:35:12 -0300

    STYLE: Refatoração dos styles dos components AdminCard, ProductManagement & SalesReports

--- src/components/admin/AdminCard.vue
+++ src/components/admin/AdminCard.vue
@@ -5,18 +5,16 @@
     role="region"
     :aria-busy="loading"
   >
-    <header class="card-header">
-      <h3 class="card-title">{{ title }}</h3>
-      <div class="card-actions">
-        <slot name="header-actions"></slot>
-      </div>
+    <header class="admin-card__header">
+      <h3 class="admin-card__title">{{ title }}</h3>
+      <slot name="header-actions"></slot>
     </header>
 
-    <section class="card-content">
+    <section class="admin-card__content">
       <slot></slot>
     </section>
 
-    <footer v-if="$slots.footer" class="card-footer">
+    <footer v-if="$slots.footer" class="admin-card__footer">
       <slot name="footer"></slot>
     </footer>
   </article>
